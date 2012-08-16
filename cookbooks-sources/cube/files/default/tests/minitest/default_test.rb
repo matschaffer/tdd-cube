1289746924feac25fcb16a1294807a34e89309f1
Mat Schaffer <mat@schaffer.me>
2012-08-16 09:09:51 -0400

    Switch to describe_recipe helper

--- cookbooks-sources/cube/files/default/tests/minitest/default_test.rb
+++ cookbooks-sources/cube/files/default/tests/minitest/default_test.rb
@@ -1,7 +1,19 @@
-class DefaultTest < MiniTest::Chef::Spec
-  describe 'cube service' do
-    it 'is running' do
-      true.must_equal false
+require 'json'
+require 'net/http'
+require 'uri'
+
+describe_recipe 'cube::default' do
+  describe 'evaluator' do
+    it 'can get types' do
+      uri = URI.parse('http://localhost:1081/1.0/types/get')
+      Net::HTTP.get_response(uri).code.should == 200
+    end
+  end
+
+  describe 'collector' do
+    it 'can show a dashboard' do
+      uri = URI.parse('http://localhost:1080/')
+      Net::HTTP.get_response(uri).code.should == 200
     end
   end
 end
