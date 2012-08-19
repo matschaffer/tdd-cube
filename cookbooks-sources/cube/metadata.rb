b93c7662f50d29422b98f2fed64db79215f97471
Mat Schaffer <mat@schaffer.me>
2012-08-18 22:29:27 -0400

    Getting started with test-kitchen

--- cookbooks-sources/cube/metadata.rb
+++ cookbooks-sources/cube/metadata.rb
@@ -5,4 +5,7 @@ description      "Installs/Configures cube"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 
+supports 'ubuntu'
+
 depends 'minitest-handler'
+
